28e2fcf1dafb7fdcef24ba1826d1a0f938b224d6
Mary Beth Smrtic <mbsmrtic@flashbit.com>
2012-11-27 21:17:33 -0500

    Added a sample vcf file and testvcffile with unit tests for VcfFile

---
+++ scripts/testvcffile.py
@@ -0,0 +1,50 @@
+import unittest
+import vcffile
+import os
+
+DATASETDIR = 'C:/Users/Mary Beth/Documents/Contests/Crohn_disease_andre_franke/data/'
+TESTDATADIR = '../data/'        
+SAMPLEFILENAME = TESTDATADIR + 'A0024_hg19.gatk.flt.vcf'
+
+class TestVcfFile(unittest.TestCase):
+    '''
+    Tests for vcffile.
+    '''
+
+    def test_person_id(self):
+        '''VcfFile.get_person_id should pull the person id from the file name'''
+        self.assertTrue(os.path.exists(SAMPLEFILENAME))
+        inputfile = vcffile.VcfFile(SAMPLEFILENAME)
+        personid = inputfile.get_person_id()
+        self.assertEqual("A0024", personid)
+
+    def test_get_first_snp_line(self):
+        '''VcfFile.get_first_snp_line should return the first snp in the file'''
+        self.assertTrue(os.path.exists(SAMPLEFILENAME))
+        inputfile = vcffile.VcfFile(SAMPLEFILENAME)
+        firstSnpLine = inputfile.get_first_snp_line()
+        firstSnp = inputfile.get_a_snp_id(firstSnpLine)
+        self.assertEqual('rs12028261', firstSnp)
+
+    def test_get_an_allele(self):
+        '''VcfFile.get_an_allele should return the snps allele'''
+        self.assertTrue(os.path.exists(SAMPLEFILENAME))
+        inputfile = vcffile.VcfFile(SAMPLEFILENAME)
+        firstSnpLine = inputfile.get_first_snp_line()
+        firstSnpAllele = inputfile.get_an_allele(firstSnpLine)
+        self.assertEqual('A', firstSnpAllele)
+        
+    def test_get_an_allele_number(self):
+        '''get_an_allele_number should convert from a character allele to a number'''
+        inputfile = vcffile.VcfFile()
+        alleleNumber = inputfile.get_an_allele_number('A', 'G')
+        self.assertEqual('1', alleleNumber)
+        alleleNumber = inputfile.get_an_allele_number('G', 'G')
+        self.assertEqual('4', alleleNumber)
+
+def main():
+    unittest.main()
+
+if __name__ =='__main__':
+    main()
+    
--- scripts/vcffile.py
+++ scripts/vcffile.py
@@ -13,26 +13,28 @@ class VcfFile():
 
     import risksnps
 
-    def __init__(self,filename):
+    def __init__(self,filename = ""):
         self.filename = filename
     
     def get_first_snp_line(self):
-        '''Reads the file until it encounters a line with a snp id (rsxxx) and
-           returns the line containing the snp or blank if none are found.
+        '''
+        Reads the file until it encounters a line with a snp id (rsxxx) and
+        returns the line containing the snp or blank if none are found.
         '''
         with open(self.filename, 'r') as snp_file:
             #using with so that the file doesn't get left open it's automatically closed when
             #we leave this code block
             for a_line in snp_file:
-                snpId = self.get_a_snp_id(a_line)   #todo - is this the correct syntax for this?
+                snpId = self.get_a_snp_id(a_line)
                 if (snpId):
                     return(a_line)
             pass
         return("")
 
     def get_these_snps(self, riskSnps):
-        '''Reads the file looking for the snps in the riskSnps argument.
-           Returns an array of the allele numbers for the snps that were found.
+        '''
+        Reads the file looking for the snps in the riskSnps argument.
+        Returns an array of the allele numbers for the snps that were found.
         '''
         alleles = ["#N/A"]*(riskSnps.len())
         with open(self.filename, 'r') as snp_file:
@@ -47,7 +49,8 @@ class VcfFile():
         return alleles
 
     def get_all_snps_and_alleles(self):
-        '''Reads the file and returns a list containing all the snp, allele combinations.
+        '''
+        Reads the file and returns a list containing all the snp, allele combinations.
         '''
         alldata = []
         index = 0
@@ -61,8 +64,9 @@ class VcfFile():
         return alldata
                     
     def get_a_snp_id(self, line):
-        '''Looks for a snp id, a string starting with rs. If one is found, it's
-           returned, otherwise an empty string.
+        '''
+        Looks for a snp id, a string starting with rs. If one is found, it's
+        returned, otherwise an empty string.
         '''
         split = line.split();
         if (split.__len__() < 3):
@@ -74,7 +78,8 @@ class VcfFile():
             return('')
 
     def get_an_allele_number(self, allele, riskAllele):
-        '''Returns a number that represents the allele.
+        '''
+        Returns a number that represents the allele.
 
         We convert it to a number so that the risk alleles are given the
         highest value (4), nas are still nas and everything else falls between those two.
@@ -92,7 +97,8 @@ class VcfFile():
         
         
     def get_an_allele(self, line):
-        '''Returns the allele from this line.  
+        '''
+        Returns the allele from this line.  
         '''
         split = line.split();
         if (split.__len__() < 5):
@@ -101,7 +107,8 @@ class VcfFile():
         return allele;
 
     def get_person_id(self):
-        '''Returns the person id from the filename.
+        '''
+        Returns the person id from the filename.
         '''
         #get the actual filename from the directory listing
         parts = self.filename.split('/')
